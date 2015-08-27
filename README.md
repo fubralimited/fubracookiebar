31f0df7fb7da98143c64d3505d81e943d2ea17ec
RayViljoen <ray@fubra.com>
2015-08-27 15:52:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Fubra Cookie Bar
+
+---
+
+## Building distribution files
+
+```
+cd path/to/fubracookiebar
+npm install
+npm install -g gulp
+gulp build
+```
+
+
+## Adding cookies bar to page
+```
+<script>
+  window.fubracookiesbar_options = {
+      message: 'This website uses cookies to ensure you get the best experience. ',
+      link: null,
+      learnMore: 'Learn More',
+      dismiss: 'Got it!'
+  }
+</script>
+<script defer type="text/javascript" src="path/to/dist/fubracookiebar.min.js"></script>
+```
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var replace = require("gulp-replace");
+
+gulp.task('sass', function () {
+  gulp.src('./styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify', ['sass'], function () {
+
+  var fs = require("fs");
+  var themeData=fs.readFileSync("./dist/theme.css", "utf8");
+
+  var minifyPipe =
+    gulp.src('./fubracookiebar.js')
+    .pipe(replace('<%theme-css%>', themeData))
+    .pipe(uglify())
+    .pipe(rename('fubracookiebar.min.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['minify']);
+gulp.task('default', ['build']);
